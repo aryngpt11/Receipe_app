ee7d93f0cc4922044dcf116303bb62464d48fe84
aryngpt11 <aryan11gs@gmail.com>
2024-06-02 00:12:19 +0530

    Adding Models

--- vege/models.py
+++ vege/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Reciepe(models.Model):
+    user=models.ForeignKey(User ,on_delete=models.SET_NULL,null=True,blank=True)
+    receipe_name=models.CharField(max_length=100)
+    recepie_description=models.TextField()
+    receipe_image=models.ImageField(upload_to="receipe")
+    receipe_views_count=models.IntegerField(default=1)
+    
